be8271a8da720bf178eb450f295c201a649598b4
realkoder <alex8970@stud.kea.dk>
2024-02-06 11:10:22 +0100

    Almost solved binary search in dictionary but some words cant be searched for...

--- binarySearchDictionary/BinarySearchWithinDictionary.js
+++ binarySearchDictionary/BinarySearchWithinDictionary.js
@@ -2,8 +2,6 @@
 
 const fs = require("fs");
 
-const stream = require("stream");
-
 let fileStream = fs.createReadStream("binarySearchDictionary/data/ddo_fullforms_2023-10-11.csv");
 
 let fileData = "";
@@ -24,9 +22,43 @@ fileStream.on("end", () => {
             partofspeech: parts[3],
             id: parts[4]
         }
-    }); 
+    });
     console.log("LINE 28: ", globalArrayOfWords.length);
-    
-    
+    console.log("LINE 28: ", globalArrayOfWords[348363]);
+
+    globalArrayOfWords = globalArrayOfWords.sort((a, b) => a.variant - b.variant);
+
+    const searchWord = "sidelomme";
+    console.log(`Index for "${searchWord}": `, binarySearchFunction({ variant: searchWord }, globalArrayOfWords, compareWordVariant));
 
 });
+
+
+const compareWordVariant = (search, check) => {
+    console.log(`comparing ${search.variant} with ${check.variant}`);
+    return search.variant.localeCompare(check.variant);
+};
+
+const binarySearchFunction = (value, values, compare) => {
+    if (values.length === 0) return;
+
+    let start = 0;
+    let end = values.length - 1;
+    let middle = 0;
+    let comparedValue = 0;
+
+    while (start <= end) {
+        middle = Math.floor(start + ((end - start) / 2));
+        comparedValue = compare(value, values[middle]);
+
+        if (comparedValue === 0) {
+            return middle;
+        } else if (comparedValue === 1) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+
+    return -1;
+}
