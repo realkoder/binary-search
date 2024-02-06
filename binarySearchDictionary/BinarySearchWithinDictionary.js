8401943be39b37671a6c9da1727f5b2c6eac94b8
realkoder <alex8970@stud.kea.dk>
2024-02-06 20:42:46 +0100

    Added perfomance metrics for home made binary search and javascripts find method

--- binarySearchDictionary/BinarySearchWithinDictionary.js
+++ binarySearchDictionary/BinarySearchWithinDictionary.js
@@ -26,14 +26,33 @@ fileStream.on("end", () => {
     const maxGuessAmount = Math.ceil(Math.log2(globalArrayOfWords.length - 1));
     globalArrayOfWords = globalArrayOfWords.sort((a, b) => a.variant.localeCompare(b.variant));
 
-    const searchWord = "heste";
+    const searchWord = "hestevogn";
     console.log("Max iteration for finding a word within globalArrayOfWords = ", maxGuessAmount);
     console.log(`Index for "${searchWord}": `, binarySearchFunction({ variant: searchWord }, globalArrayOfWords, compareWordVariant));
     console.log("Index for searchword with js find method: ", globalArrayOfWords.findIndex(word => word.variant === searchWord));
+
+
+    // ----------------------------------------------------------------------
+    // NOW TIME FOR MEASUREMENTS
+    // Home made binary search
+    performance.mark("binary-search-start");
+    binarySearchFunction({ variant: searchWord }, globalArrayOfWords, compareWordVariant);
+    performance.mark("binary-search-end");
+    performance.measure("binary-search", "binary-search-start", "binary-search-end");
+
+    // Javascripts own find method:
+    performance.mark("find-method-start");
+    globalArrayOfWords.findIndex(word => word.variant === searchWord)
+    performance.mark("find-method-end");
+    performance.measure("find-method", "find-method-start", "find-method-end");
+
+    // Let's see the results
+    console.log("BINARY SEARCH PERFOMANCE: ", performance.getEntriesByName("binary-search"));
+    console.log("FIND METHOD PERFOMANCE: ", performance.getEntriesByName("find-method"));
 });
 
 
-const compareWordVariant = (search, check) => {    
+const compareWordVariant = (search, check) => {
     return search.variant.localeCompare(check.variant);
 };
 
@@ -41,7 +60,7 @@ const binarySearchFunction = (value, values, compare) => {
     if (values.length === 0) return;
 
     let start = 0;
-    let end = values.length - 1;    
+    let end = values.length - 1;
 
     while (start <= end) {
         const middle = Math.floor(start + ((end - start) / 2));
